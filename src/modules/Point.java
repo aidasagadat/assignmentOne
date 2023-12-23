3543b222559fc246125e4057263333ae089c4def
Aida Sagadat <aidasagadat355@gmail.com>
2023-12-23 08:30:30 +0600

    .

---
+++ src/modules/Point.java
@@ -0,0 +1,26 @@
+package modules;
+import java.lang.Math;
+
+public class Point {
+    private double x;
+    private double y;
+
+    // constructor having both parameters
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+    // distance(Point dest) - distance from current point to the dest point
+    public double distanceTo(Point dest){
+        return Math.sqrt(Math.pow((this.x - dest.x), 2)+ Math.pow((this.y-dest.y),2));
+    }
+
+
+    // toString
+    @Override
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/modules/Shape.java
@@ -0,0 +1,19 @@
+package modules;
+
+import java.util.ArrayList;
+
+public class Shape {
+    // container of Points (e.g. ArrayList<Point>)
+    ArrayList<Point> points = new ArrayList<Point>();
+
+    // addPoint(Point) - adds to the container
+    public void addPoint(Point newPoint){
+        points.add(newPoint);
+    }
+
+    // calculatePerimeter()
+
+    // getAverageSide()
+
+    // getLongestSide()
+}
